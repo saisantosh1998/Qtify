f2991c82b21a10d957d4dfff4ea8aa84b636ac06
saisantosh1998 <58393030+saisantosh1998@users.noreply.github.com>
2024-01-28 13:36:08 +0530

    update card

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,37 +1,71 @@
 import React from "react";
 import Chip from "@mui/material/Chip";
 import styles from "./Card.module.css";
+import { Tooltip } from "@mui/material";
 
 const Card = ({ album }) => {
-  const { title, follows, image, likes } = album;
+  const { title, follows, image, likes, songs } = album;
 
   return (
-    <div className={styles.cardContainer}>
-      <div className={styles.card}>
-        <img src={image} alt={title} className={styles.cardImage} />
-        <div className={styles.cardInfo}>
-          {follows && (
-            <Chip
-              label={`${follows} Follows`}
-              sx={{
-                backgroundColor: "var(--color-black)",
-                color: "var(--color-white)",
-              }}
-            />
-          )}
-          {likes && (
-            <Chip
-              label={`${likes} Likes`}
-              sx={{
-                backgroundColor: "var(--color-black)",
-                color: "var(--color-white)",
-              }}
-            />
-          )}
-        </div>
-      </div>
-      <h3 className={styles.cardTitle}>{title}</h3>
-    </div>
+    <>
+      {songs && songs.length > 0 ? (
+        <Tooltip title={`${songs.length} Songs`} arrow placement="top">
+          <div className={styles.cardContainer}>
+            <div className={styles.card}>
+              <img src={image} alt={title} className={styles.cardImage} />
+              <div className={styles.cardInfo}>
+                {follows && (
+                  <Chip
+                    label={`${follows} Follows`}
+                    sx={{
+                      backgroundColor: "var(--color-black)",
+                      color: "var(--color-white)",
+                    }}
+                  />
+                )}
+                {likes && (
+                  <Chip
+                    label={`${likes} Likes`}
+                    sx={{
+                      backgroundColor: "var(--color-black)",
+                      color: "var(--color-white)",
+                    }}
+                  />
+                )}
+              </div>
+            </div>
+            <h3 className={styles.cardTitle}>{title}</h3>
+          </div>
+        </Tooltip>
+      ) : (
+        <div className={styles.cardContainer}>
+            <div className={styles.card}>
+              <img src={image} alt={title} className={styles.cardImage} />
+              <div className={styles.cardInfo}>
+                {follows && (
+                  <Chip
+                    label={`${follows} Follows`}
+                    sx={{
+                      backgroundColor: "var(--color-black)",
+                      color: "var(--color-white)",
+                    }}
+                  />
+                )}
+                {likes && (
+                  <Chip
+                    label={`${likes} Likes`}
+                    sx={{
+                      backgroundColor: "var(--color-black)",
+                      color: "var(--color-white)",
+                    }}
+                  />
+                )}
+              </div>
+            </div>
+            <h3 className={styles.cardTitle}>{title}</h3>
+          </div>
+      )}
+    </>
   );
 };
 
